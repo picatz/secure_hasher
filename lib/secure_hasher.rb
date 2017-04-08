ad0473332fe0f22d4664f34efa24831d35ae537a
Kent Gruber <kgruber1@emich.edu>
2017-04-07 23:52:08 -0400

    Add easter egg, updat version.

--- lib/secure_hasher.rb
+++ lib/secure_hasher.rb
@@ -1,3 +1,9 @@
 require "secure_hasher/version"
 require "secure_hasher/hasher"
 require "secure_hasher/errors"
+
+module SecureHasher
+  def self.documentation_hard?
+    true
+  end
+end
--- spec/secure_hasher_spec.rb
+++ spec/secure_hasher_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe SecureHasher do
   it "has a version number" do
     expect(SecureHasher::VERSION).not_to be nil
   end
+  
+  it "has an easter egg number" do
+    expect(SecureHasher.documentation_hard?).to be true
+  end
 
 end
